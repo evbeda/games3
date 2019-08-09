a5f3812755b899ca4d88fc9fbab878c82ad7c6a3
julian.rofrano <julian.rofrano@julian.rofrano-07322>
2019-08-09 17:41:11 -0300

    diezmil test added

--- test_suite.py
+++ test_suite.py
@@ -4,6 +4,7 @@ from craps.test_craps import TestCraps
 from dungeon_raiders.test_dungeon import TestDungeon
 from ruleta.test_ruleta import Test_Ruleta
 from sudoku.test_sudoku import TestSudoku
+from diezmil.test_diez_mil import TestDiezMil
 
 
 def suite():
@@ -13,6 +14,7 @@ def suite():
     test_suite.addTest(unittest.makeSuite(TestDungeon))
     test_suite.addTest(unittest.makeSuite(Test_Ruleta))
     test_suite.addTest(unittest.makeSuite(TestSudoku))
+    test_suite.addTest(unittest.makeSuite(TestDiezMil))
 
 
 if __name__ == '__main__':
