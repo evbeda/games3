5440fbe02c6ae523d8106d08a567f7942e5c9328
Nahuel Valencia <nahuel.valencia@nahuel.valencia-10588>
2019-08-09 16:58:23 -0300

    Added craps

---
+++ craps/tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class CrapsTests(unittest.TestCase):
+    def test_dummy(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
