c10d1b826ebb196a757d1a15a34ce9d52b22201e
Gabriel Flores <gabrielf@eventbrite.com>
2019-08-12 12:58:08 -0300

    add guess number game, example

--- test_suite.py
+++ test_suite.py
@@ -2,6 +2,7 @@ import unittest
 from uno.test_uno import TestUno
 from craps.test_craps import TestCraps
 from dungeon_raiders.test_dungeon import TestDungeon
+from guess_number_game.test_guess_number_game import TestGuessNumberGame
 from ruleta.test_ruleta import TestRuleta
 from sudoku.test_sudoku import TestSudoku
 from diezmil.test_diez_mil import TestDiezMil
@@ -15,6 +16,7 @@ def suite():
     test_suite.addTest(unittest.makeSuite(TestRuleta))
     test_suite.addTest(unittest.makeSuite(TestSudoku))
     test_suite.addTest(unittest.makeSuite(TestDiezMil))
+    test_suite.addTest(unittest.makeSuite(TestGuessNumberGame))
 
 
 if __name__ == '__main__':
