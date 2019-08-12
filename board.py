04b93ec2558a956de014eda37bf03c28cdc3ace3
ricardo.caif <ricardo.caif@ricardo.caif-10964>
2019-08-12 18:19:10 -0300

    sudoku

---
+++ board.py
@@ -0,0 +1,18 @@
+class Board():
+    def __init__(self, board):
+        self.board = board
+        
+        def build_board(board):
+            a = []
+            b = []
+            c = []
+            a = []
+            a = []
+            a = []
+            a = []
+            a = []
+            a = []
+
+
+    def unchangeable(row, column):
+        return row[column-1]['modifiable']
--- sudoku/test_sudoku.py
+++ sudoku/test_sudoku.py
@@ -1,10 +1,29 @@
 import unittest
-
+from game import Game
+from board import Board
 
 class TestSudoku(unittest.TestCase):
+    def setUp(self):
+        self.board = Board(
+            " 6 3  8 4"
+            "537 9    "
+            " 4   63 7"
+            " 9  51238"
+            "         "
+            "71362  4 "
+            "3 64   1 "
+            "    6 523"
+            "1 2  9 8 "
+        )
+    
     def test_sudoku(self):
         self.assertTrue(True)
-
-
+    
+    def test_present_numbers_unchangeable(self):
+        self.assertTrue(self.board.unchangeable('A', 2))
+        
+    def test_change_unchangeable(self):
+        self.assertEqual(self.board.play('A', 2, 4), "You can't change boards default numbers")
+    
 if __name__ == '__main__':
     unittest.main()
