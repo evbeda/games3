64f5a26700faf51e4436eeba747a9e9091e25772
ricardo.caif <ricardo.caif@ricardo.caif-10964>
2019-08-13 12:56:26 -0300

    fixin importing

--- sudoku/board.py
+++ sudoku/board.py
@@ -1,21 +1,29 @@
 class Board():
     def __init__(self, board):
         self.board = self.build_board(board)
-        
-        
+
     def build_board(self, board):
         return {
-            'a':[{'val':i, 'mod': True} if i == ' ' else {'val':i, 'mod': False} for i in board[0:9]],
-            'b':[{'val':i, 'mod': True} if i == ' ' else {'val':i, 'mod': False} for i in board[9:18]],
-            'c':[{'val':i, 'mod': True} if i == ' ' else {'val':i, 'mod': False} for i in board[18:27]],
-            'd':[{'val':i, 'mod': True} if i == ' ' else {'val':i, 'mod': False} for i in board[27:36]],
-            'e':[{'val':i, 'mod': True} if i == ' ' else {'val':i, 'mod': False} for i in board[36:45]],
-            'f':[{'val':i, 'mod': True} if i == ' ' else {'val':i, 'mod': False} for i in board[45:54]],
-            'g':[{'val':i, 'mod': True} if i == ' ' else {'val':i, 'mod': False} for i in board[54:63]],
-            'h':[{'val':i, 'mod': True} if i == ' ' else {'val':i, 'mod': False} for i in board[63:72]],
-            'i':[{'val':i, 'mod': True} if i == ' ' else {'val':i, 'mod': False} for i in board[72:81]]
+            'a':
+            [{'val':i, 'mod':True} if i == ' ' else {'val':i, 'mod':False} for i in board[0:9]],
+            'b':
+            [{'val':i, 'mod':True} if i == ' ' else {'val':i, 'mod':False} for i in board[9:18]],
+            'c':
+            [{'val':i, 'mod':True} if i == ' ' else {'val':i, 'mod':False} for i in board[18:27]],
+            'd':
+            [{'val':i, 'mod':True} if i == ' ' else {'val':i, 'mod':False} for i in board[27:36]],
+            'e':
+            [{'val':i, 'mod':True} if i == ' ' else {'val':i, 'mod':False} for i in board[36:45]],
+            'f':
+            [{'val':i, 'mod':True} if i == ' ' else {'val':i, 'mod':False} for i in board[45:54]],
+            'g':
+            [{'val':i, 'mod':True} if i == ' ' else {'val':i, 'mod':False} for i in board[54:63]],
+            'h':
+            [{'val':i, 'mod':True} if i == ' ' else {'val':i, 'mod':False} for i in board[63:72]],
+            'i':
+            [{'val':i, 'mod':True} if i == ' ' else {'val':i, 'mod':False} for i in board[72:81]]
         }
-    
+
     def unchangeable(self, row, column):
         board_row = self.board[row.lower()]
         board_colum = int(column-1)
