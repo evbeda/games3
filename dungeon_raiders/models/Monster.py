3dbbb77398350d88158354d715ff223d5a17df05
facundo.chaud <facundo.chaud@facundo.chaud-07082>
2019-08-13 10:52:48 -0300

    Fight against monster tests

--- dungeon_raiders/models/Monster.py
+++ dungeon_raiders/models/Monster.py
@@ -1,14 +1,14 @@
-from models.Player import Player
-
-
 class Monster:
 
     def __init__(self, life, damage):
         self.life = life
         self.damage = damage
 
-    # def resolve_room(self, players):
-    #     total_damage = sum([player.actual_card for player in players])
-    #     if total_damage < self.life :
-            
-    
+    def resolve_room(self, hands):
+        played_cards = [hand.last_card_played for hand in hands]
+        total_damage = sum(played_cards)
+        if total_damage < self.life:
+            min_card = min(played_cards)
+            for hand in hands:
+                if hand.last_card_played == min_card:
+                    hand.player.add_wounds(self.damage)
