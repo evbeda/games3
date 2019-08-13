fc55646eae33d0a5532d890d420d7547def8f4f7
juan.vallejo <juan.vallejo@juan.vallejo-06037>
2019-08-13 09:54:15 -0300

    added straight bet with test

--- ruleta/bet.py
+++ ruleta/bet.py
@@ -1,10 +1,13 @@
 
 
-class Bet:
-    def __init__(self, bet_type, numbers, ammount):
+class BetCreator:
+    def create(self, bet_type, numbers, ammount):
         bet = None
-        if(bet_type == 1 and StraightBet.validate_straight(numbers)):
-            bet = StraightBet(numbers, ammount)
+        if(bet_type == 1):
+            if(StraightBet.validate_straight(numbers)):
+                bet = StraightBet(numbers, ammount)
+            else:
+                bet = "Error, invalid bet"
         return bet
 
 
@@ -13,6 +16,7 @@ class StraightBet:
         self.reward = 35
         self.ammount = ammount
 
+    @staticmethod
     def validate_straight(number):
         if 0 <= number and number <= 36:
             return True
