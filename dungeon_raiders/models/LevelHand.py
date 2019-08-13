c73c622475a0a76b3f44012bc3a84613ca347327
nicolas.nunez <nicolas.nunez@nicolas.nunez-07221>
2019-08-13 09:57:06 -0300

    Adding LevelHand class and refactor the tests

---
+++ dungeon_raiders/models/LevelHand.py
@@ -0,0 +1,17 @@
+from models.exceptions.UnplayableCardException import UnplayableCardException
+
+
+class LevelHand:
+    def __init__(self, player):
+        self.cards_to_play = [1, 2, 3, 4, 5]
+        self.player = player
+        self.last_card_played = 0
+
+    def play(self, card_to_play):
+        self.validate(card_to_play)
+        self.cards_to_play.remove(card_to_play)
+        self.last_card_played = card_to_play
+
+    def validate(self, card):
+        if card not in self.cards_to_play:
+            raise UnplayableCardException(f'You can\'t play {card} again')
