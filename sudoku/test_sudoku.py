4d85ff1bc2b96b130dc5798b84af1aa8651460b2
ricardo.caif <ricardo.caif@ricardo.caif-10964>
2019-08-13 10:34:25 -0300

    created test and code to verify unchangable property

--- sudoku/test_sudoku.py
+++ sudoku/test_sudoku.py
@@ -1,10 +1,16 @@
 import unittest
-
+# from game import Game
+from board import Board
 
 class TestSudoku(unittest.TestCase):
     def test_sudoku(self):
         self.assertTrue(True)
-
-
+    
+    def test_present_numbers_unchangeable(self):
+        self.assertTrue(self.board.unchangeable('A', 3))
+        
+    # def test_change_unchangeable(self):
+    #     self.assertEqual(self.board.play('A', 2, 4), "You can't change boards default numbers")
+    
 if __name__ == '__main__':
     unittest.main()
