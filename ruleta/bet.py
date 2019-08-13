5f2c4c6ad5c0ffb6d59355ec010312a4783a2af9
nicolas.nunez <nicolas.nunez@nicolas.nunez-07221>
2019-08-13 17:20:57 -0300

    Exceptions added, straight_bet tests changed. place_bet method and dicrement_money methods from Player class were refactored, validate method from StraightBet class was refactor and create method from BetCreator class was refactor too

--- ruleta/bet.py
+++ ruleta/bet.py
@@ -1,30 +1,39 @@
+from .exceptions.invalid_bet_exception import InvalidBetException
+
+
+class StraightBet:
+    def __init__(self, bet_value, ammount):
+        self.reward = 35
+        self.ammount = ammount
+        self.bet_value = bet_value
+        self.validate()
+
+    def validate(self):
+        ''' expect bet_value like "1" '''
+        if not (0 <= self.bet_value <= 36):
+            raise InvalidBetException('Invalid bet')
+
+
+bet_types = {
+    1: StraightBet,
+}
 
 
 class BetCreator:
-    def create(self, bet_type, numbers, ammount):
+    def create(self, bet_type, bet_value, ammount):
         bet = None
-        if(bet_type == 1):
-            if(StraightBet.validate_straight(numbers)):
-                bet = StraightBet(numbers, ammount)
-            elif(bet_type == 0):
-                bet = "END"
-            else:
-                bet = "Error, invalid bet"
+        if bet_type in bet_types:
+            try:
+                bet_class = bet_types[bet_type]
+                bet = bet_class(bet_value, ammount)
+            except InvalidBetException:
+                print('Your bet value is invalid ')
+        elif bet_type == 0:
+            bet = 'END'
+        else:
+            bet = "Error, invalid bet"
         return bet
 
     @staticmethod
     def list_bets():
         pass
-
-
-class StraightBet:
-    def __init__(self, number, ammount):
-        self.reward = 35
-        self.ammount = ammount
-
-    @staticmethod
-    def validate_straight(number):
-        if 0 <= number and number <= 36:
-            return True
-        else:
-            return False
