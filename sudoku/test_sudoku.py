4fd3dc3359110f42e92ee89c718203be41a384fa
ricardo.caif <ricardo.caif@ricardo.caif-10964>
2019-08-13 11:51:05 -0300

    Added test, class and methods to verify unchangeable condition of default values in board

--- sudoku/test_sudoku.py
+++ sudoku/test_sudoku.py
@@ -3,11 +3,24 @@ import unittest
 from board import Board
 
 class TestSudoku(unittest.TestCase):
+    def setUp(self):
+        self.board = Board(
+            " 6 3  8 4"
+            "537 9    "
+            " 4   63 7"
+            " 9  51238"
+            "         "
+            "71362  4 "
+            "3 64   1 "
+            "    6 523"
+            "1 2  9 8 "
+        )
+    
     def test_sudoku(self):
         self.assertTrue(True)
     
     def test_present_numbers_unchangeable(self):
-        self.assertTrue(self.board.unchangeable('A', 3))
+        self.assertFalse(self.board.unchangeable('A', 2))
         
     # def test_change_unchangeable(self):
     #     self.assertEqual(self.board.play('A', 2, 4), "You can't change boards default numbers")
