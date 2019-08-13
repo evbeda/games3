cfee08ee4e3c0163bcee41eec10cadbd18cc0d2e
facundo.chaud <facundo.chaud@facundo.chaud-07082>
2019-08-13 10:52:48 -0300

    Fight against monster tests

--- dungeon_raiders/models/Player.py
+++ dungeon_raiders/models/Player.py
@@ -1,11 +1,8 @@
 class Player:
     def __init__(self, name):
         self.name = name
-        self.power_cards = [1, 2, 3, 4, 5]
         self.wounds = 0
         self.gold = 0
 
-    # def __eq__(self, player):
-    #     return self.name == player.name
-
-      
+    def add_wounds(self, wounds):
+        self.wounds += wounds
