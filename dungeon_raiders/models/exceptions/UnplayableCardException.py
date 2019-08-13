beddc7d635990dcf9ffcced64752b4ac9f4230a0
nicolas.nunez <nicolas.nunez@nicolas.nunez-07221>
2019-08-13 09:26:09 -0300

    Implementation and testing for the method play of Player and method get_winner for Game

---
+++ dungeon_raiders/models/exceptions/UnplayableCardException.py
@@ -0,0 +1,2 @@
+class UnplayableCardException(Exception):
+    pass
--- dungeon_raiders/test_dungeon.py
+++ dungeon_raiders/test_dungeon.py
@@ -1,9 +1,47 @@
 import unittest
+from models.Player import Player
+from models.Game import Game
+from models.exceptions.UnplayableCardException import UnplayableCardException
 
 
 class TestDungeon(unittest.TestCase):
-    def test_dummy(self):
-        self.assertTrue(True)
+    # Test del jugador
+    def test_init_player(self):
+        player = Player('A')
+        self.assertEqual(5, len(player.power_cards))
+
+    def test_jugador_con_cartas_431_no_puede_jugar2(self):
+        player = Player('A')
+        player.play(2)
+        player.play(5)
+        with self.assertRaises(UnplayableCardException):
+            player.play(2)
+
+    def test_actual_card_es_2_si_se_jugo_un2(self):
+        player = Player('A')
+        player.play(2)
+        self.assertEqual(2, player.actual_card)
+
+    def test_si_se_jugo_un3_no_hay_un_3_en_la_mano(self):
+        player = Player('A')
+        player.play(3)
+        self.assertTrue(3 not in player.power_cards)
+
+    # Game
+    def test_gana_el_jugador_A(self):
+        playerA = Player('A')
+        playerB = Player('B')
+        playerC = Player('C')
+        playerA.gold = 50
+        playerA.wounds = 10
+        playerB.gold = 40
+        playerB.wounds = 15
+        playerC.gold = 60
+        playerC.wounds = 20
+        game = Game([playerA, playerB, playerC])
+        self.assertEqual(playerA, game.get_winner())
+
+    # Room card
 
 
 if __name__ == '__main__':
