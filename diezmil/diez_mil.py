7b894a621d4abff4377cfeb7daa6e9feee262a6f
julian.rofrano <julian.rofrano@julian.rofrano-07322>
2019-08-13 09:27:15 -0300

    added triple same dice score

--- diezmil/diez_mil.py
+++ diezmil/diez_mil.py
@@ -13,11 +13,16 @@ class DiezMil(object):
         return True
     def checkCombination(self, play):
         totalScore=0
+        isRepeated = False
         for i in play:
+            quantity = play.count(i)
             if i == 1:
                 totalScore += 100
             elif i == 5:
-                totalScore += 50    
-                  
+                totalScore += 50 
+            if quantity == 3 and isRepeated == False:
+                totalScore += i*100
+                isRepeated = True
+
         return totalScore
 
--- diezmil/test_diez_mil.py
+++ diezmil/test_diez_mil.py
@@ -9,15 +9,15 @@ class TestDiezMil(unittest.TestCase):
         checkPlayersQty=self.game.checkPlayersQty(0)
         self.assertEquals(checkPlayersQty, False)    
     def test_check_combination_no_score(self):
-        dice=[2,3,3,4,3]
+        dice=[2,3,3,4,4]
         score=self.game.checkCombination(dice)
         self.assertEquals(score, 0)
     
     def test_check_combination_score(self):
-        dice=[1,3,3,4,5]
+        dice=[1,3,3,5,3]
         score=self.game.checkCombination(dice)
-        self.assertEquals(score, 150)
-        
+        self.assertEquals(score, 450)
+
 
 
 if __name__ == '__main__':
