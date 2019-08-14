23a9448017b1a44c81c5e399cef088cbaa8fbc48
Walter Bucolo <walter.bucolo@eventbrite.com>
2019-08-14 16:00:03 -0300

    some structure problems resolved

--- ruleta/bet.py
+++ ruleta/bet.py
@@ -1,28 +1,80 @@
 from .exceptions.invalid_bet_exception import InvalidBetException
 from .exceptions.invalid_bet_type_exception import InvalidBetTypeException
 
+all_values = list(range(37))
 
-class StraightBet:
-    name = 'STRAIGHT_BET\n'
 
-    def __init__(self, bet_value, ammount, player):
-        self.reward = 35
+class Bet:
+    # reward = 0
+
+    def __init__(self, bet_value, ammount):
         self.ammount = ammount
-        self.bet_value = bet_value[0]
-        self.validate()
-        self.player = player
+        self.target_numbers = bet_value
 
-    def validate(self):
-        ''' expect bet_value like "1" '''
-        if not (0 <= self.bet_value <= 36):
-            raise InvalidBetException()
+    def is_winner(self, chosen_number):
+        return chosen_number in self.target_numbers
 
     def calculate_total_award(self):
         return self.reward * self.ammount
 
-    def win_bet(self, chosen_number):
-        if chosen_number == self.bet_value:
-            self.player.money += self.calculate_total_award()
+
+class StraightBet(Bet):
+    name = 'STRAIGHT_BET\n'
+    reward = 35
+
+    def __init__(self, bet_value, ammount):
+        self.ammount = ammount
+        self.target_numbers = bet_value
+        self.validate(bet_value[0])
+
+    def validate(self, bet_value):
+        ''' expect bet_value like "1" '''
+        import ipdb; ipdb.set_trace()
+        if not (0 <= bet_value <= 36):
+            raise InvalidBetException()
+
+
+class ColorBet(Bet):
+    name = 'COLOR_BET\n'
+    reward = 2
+
+    def __init__(self, bet_value, ammount):
+        self.validate(bet_value[0])
+        self.ammount = ammount
+        self.target_numbers = self.transform_bet_value_to_target_values(bet_value)
+        # super().__init__(self.transform_bet_value_to_target_values(bet_value),
+        #                  ammount)
+
+    def transform_bet_value_to_target_values(self, bet_value):
+        range_1 = [number for number in all_values if number in range(1, 11)
+                   and number % 2 == 1]
+        range_2 = [number for number in all_values if number in range(19, 29)
+                   and number % 2 == 1]
+        range_3 = [number for number in all_values if number in range(11, 19)
+                   and number % 2 == 0]
+        range_4 = [number for number in all_values if number in range(29, 37)
+                   and number % 2 == 0]
+        red = range_1 + range_2 + range_3 + range_4
+        if bet_value[0].lower() == 'red':
+            return red
+        else:
+            return list(set(all_values) - set(red))
+
+    def validate(self, bet_value):
+        ''' expect bet_value like "Red" or "Black" '''
+        if bet_value.lower() not in ['red', 'black']:
+            raise InvalidBetException()
+
+
+class EvenOddBet(Bet):
+    name = 'EVEN_ODD_BET\n'
+    reward = 2
+    # to do same as in ColorBet
+
+    def validate(self):
+        ''' expect bet_value like "Even" or "Odd" '''
+        if self.bet_value.lower() not in ['even', 'odd']:
+            raise InvalidBetException()
 
 
 bet_types = {
@@ -31,10 +83,11 @@ bet_types = {
 
 
 class BetCreator:
-    def create(self, bet_type, bet_values, ammount, player):
+
+    def create(self, bet_type, bet_values, ammount):
         bet = None
         bet_class = bet_types[bet_type]  # obtain bet Class from dictionary
-        bet = bet_class(bet_values, ammount, player)
+        bet = bet_class(bet_values, ammount)
         return bet
 
     def validate_bet_type(bet_type):
