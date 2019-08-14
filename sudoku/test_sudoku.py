40ed984909c4e2ec0c090deef9cf49453bd16580
facundo.chaud <facundo.chaud@facundo.chaud-07082>
2019-08-14 11:11:03 -0300

    validate rows

--- sudoku/test_sudoku.py
+++ sudoku/test_sudoku.py
@@ -1,6 +1,8 @@
 import unittest
+from parameterized import parameterized
 # from game import Game
 from .board import Board
+
 
 class TestSudoku(unittest.TestCase):
     def setUp(self):
@@ -15,15 +17,40 @@ class TestSudoku(unittest.TestCase):
             "    6 523"
             "1 2  9 8 "
         )
-    
+
     def test_sudoku(self):
         self.assertTrue(True)
-    
+
     def test_existing_numbers_are_not_modifiable(self):
         self.assertFalse(self.board.is_modifiable('A', 2))
-        
-    # def test_change_unchangeable(self):
-    #     self.assertEqual(self.board.play('A', 2, 4), "You can't change boards default numbers")
-    
+
+    @parameterized.expand([
+        ('a', 3),
+        ('b', 7),
+        ('c', 6),
+        ('d', 5),
+        ('f', 1),
+        ('g', 4),
+        ('h', 2),
+        ('i', 9)
+    ])
+    def test_validate_insert_illegal_value_in_row(self, row, value):
+        self.assertFalse(self.board.validate_row(row, value))
+
+    @parameterized.expand([
+        ('a', 1),
+        ('b', 2),
+        ('c', 5),
+        ('d', 4),
+        ('e', 3),
+        ('f', 8),
+        ('g', 7),
+        ('h', 9),
+        ('i', 6)
+    ])
+    def test_validate_insert_legal_value_in_row(self, row, value):
+        self.assertTrue(self.board.validate_row(row, value))
+
+
 if __name__ == '__main__':
     unittest.main()
