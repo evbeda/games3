8de11753b2429abef8ed0e04df4e69160baf17f0
facundo.chaud <facundo.chaud@facundo.chaud-07082>
2019-08-15 09:26:52 -0300

    add test suites

--- test_suite.py
+++ test_suite.py
@@ -2,6 +2,7 @@ import unittest
 from uno.test_uno import TestUno
 from craps.test_craps import TestCraps
 from craps.test_turn import TestTurn
+from craps.test_bets import TestBets
 from dungeon_raiders.test_dungeon import TestDungeon
 from guess_number_game.test_guess_number_game import TestGuessNumberGame
 from ruleta.test_ruleta import TestRuleta
@@ -11,13 +12,21 @@ from diezmil.test_diez_mil import TestDiezMil
 
 def suite():
     test_suite = unittest.TestSuite()
+    # UNO
     test_suite.addTest(unittest.makeSuite(TestUno))
+    # CRAPS
     test_suite.addTest(unittest.makeSuite(TestCraps))
     test_suite.addTest(unittest.makeSuite(TestTurn))
+    test_suite.addTest(unittest.makeSuite(TestBets))
+    # DUNGEON RAIDERS
     test_suite.addTest(unittest.makeSuite(TestDungeon))
+    # ROULETTE
     test_suite.addTest(unittest.makeSuite(TestRuleta))
+    # SUDOKU
     test_suite.addTest(unittest.makeSuite(TestSudoku))
+    # DIEZ MIL
     test_suite.addTest(unittest.makeSuite(TestDiezMil))
+    # GUESS NUMBER GAME
     test_suite.addTest(unittest.makeSuite(TestGuessNumberGame))
 
 
