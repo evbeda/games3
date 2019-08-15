fa4ee8b90b9f09cb4ebd0181d64d6cfc186e5e11
facundo.chaud <facundo.chaud@facundo.chaud-07082>
2019-08-15 11:24:10 -0300

    craps added user input bets

--- craps/game.py
+++ craps/game.py
@@ -1,10 +1,14 @@
 from .turn import Turn
+from .bet import BetCreator
+from .exceptions.invalid_bet_exception import InvalidBetTypeException
 from .constants import (
     PLAYER_LOST,
     PLAYER_WON,
     WON_MESSAGE,
     LOST_MESSAGE,
     BET_MESSAGE,
+    BET_PLACED,
+    INVALID_BET_TYPE
 )
 
 
@@ -33,8 +37,24 @@ class CrapsGame:
             turn_score = self.turn.shoot()
             return turn_score
         else:
-            pass
-            # input bet
+            try:
+                bet_type, amount, bet_values = self.resolve_command(user_input)
+                bet = BetCreator.create(bet_type, amount, bet_values)
+                self.bets.append(bet)
+                return BET_PLACED + bet_type
+            except InvalidBetTypeException:
+                return INVALID_BET_TYPE
+
+    # command like
+    # BETNAME amount dice_options
+    def resolve_command(self, command):
+        list_string = command.split()
+        bet_type = list_string[0]
+        amount = int(list_string[1])
+        bet_values = [int(number) for number in list_string[2:]]
+        if len(bet_values) > 2:
+            bet_values = bet_values[0:2]
+        return (bet_type, amount, bet_values)
 
     # @property
     # def board(self):
--- craps/test_craps.py
+++ craps/test_craps.py
@@ -11,6 +11,8 @@ from .constants import (
     WON_MESSAGE,
     LOST_MESSAGE,
     BET_MESSAGE,
+    BET_PLACED,
+    INVALID_BET_TYPE
 )
 
 
@@ -52,3 +54,25 @@ class TestCraps(unittest.TestCase):
     def test_craps_play_returns_score(self, dice):
         with patch('random.sample', return_value=dice):
             self.assertEqual(self.game.play('Go'), dice)
+
+    @parameterized.expand([
+        ("PASS_BET 10", ("PASS_BET", 10, [])),
+        ("PASS_BET 100 5", ("PASS_BET", 100, [5])),
+        ("DO_NOT_PASS_BET 10 2 4", ("DO_NOT_PASS_BET", 10, [2, 4])),
+        ("PASS_BET 10 3 4 1 5", ("PASS_BET", 10, [3, 4]))
+    ])
+    def test_craps_game_input_commands(self, input, result):
+        self.assertEqual(self.game.resolve_command(input), result)
+
+    def test_craps_game_input_bet_placed_message(self):
+        returned_play = self.game.play("PASS_BET 10")
+        self.assertEqual(returned_play, BET_PLACED + "PASS_BET")
+
+    def test_craps_game_bet_added_to_bets_list(self):
+        self.game.play("PASS_BET 10")
+        self.game.play("DO_NOT_PASS_BET 20")
+        self.assertEqual(len(self.game.bets), 2)
+
+    def test_craps_game_invalid_bet_type(self):
+        returned_play = self.game.play("INVALIDBET 5678")
+        self.assertEqual(returned_play, INVALID_BET_TYPE)
