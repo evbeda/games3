c5ab5323ccd3893aafb60aa6fa8c33ae73530404
dbertranou-eb <daniela.bertranou@daniela.bertranou-10963>
2019-08-16 10:47:59 -0300

    refactored card validation structure

--- uno/card.py
+++ uno/card.py
@@ -1,19 +1,7 @@
-# Color: red, yellow, blue and green
-
-# Normal cards: from 0 to 9, 2 cards of each color and number
-
-# Special cards
-# - reverse: 2 of each color
-# - skip: 2 of each color
-# - drawTwo: 2 of each color
-# - drawFour: total 4
-# - wildCard: total 4
 
 
 class Card():
-
-    def same_type_validator(self, card):
-        return type(self) == type(card)
+    pass
 
 
 class PostColoredCard(Card):
@@ -24,7 +12,7 @@ class PostColoredCard(Card):
     def set_color(self, color):
         self.color = color
 
-    def same_to(self, card):
+    def is_valid(self, card):
         return True
 
 
@@ -33,8 +21,14 @@ class ColoredCard(Card):
     def __init__(self, color):
         self.color = color
 
-    def same_to(self, card):
+    def same_color(self, card):
         return self.color == card.color
+
+    def same_type(self, card):
+        return type(self) == type(card)
+
+    def is_valid(self, card):
+        return self.same_color(card) or self.same_type(card)
 
 
 class NumberCard(ColoredCard):
@@ -43,13 +37,13 @@ class NumberCard(ColoredCard):
         super().__init__(color)
         self.number = number
 
-    def same_to(self, card):
-
-        if super().same_to(card):
-            return True
-
-        if isinstance(card, NumberCard):
+    def same_number(self, card):
+        if self.same_type(card):
             return card.number == self.number
+        return False
+
+    def is_valid(self, card):
+        return self.same_color(card) or self.same_number(card)
 
 
 class ReverseCard(ColoredCard):
@@ -69,6 +63,7 @@ class DrawTwoCard(ColoredCard):
         self.color = color
         self.cardsToDraw = 2
 
+# TODO This should check the stack, not a previous card. Check camel_case.
     def action(self, previousCard):
         if type(previousCard) == DrawTwoCard:
             self.cardsToDraw += previousCard.cardsToDraw
--- uno/test_uno.py
+++ uno/test_uno.py
@@ -1,6 +1,6 @@
 import unittest
 from parameterized import parameterized
-from .const import RED, YELLOW
+from .const import RED, YELLOW, GREEN
 from .card import (
     NumberCard,
     ReverseCard,
@@ -13,7 +13,6 @@ from .card import (
 
 class TestUno(unittest.TestCase):
 
-    # Falta dividir los test cases
     @parameterized.expand([
         (SkipCard(RED), NumberCard(RED, '1')),
         (SkipCard(RED), SkipCard(RED)),
@@ -32,12 +31,12 @@ class TestUno(unittest.TestCase):
         (NumberCard(RED, '0'), ReverseCard(RED)),
         (NumberCard(RED, '0'), DrawTwoCard(RED)),
     ])
-    def test_same_to_colored_card_valid_color_to_colored_card(
+    def test_same_color_valid_color_to_colored_card(
         self,
         selected_card,
         top_card
     ):
-        self.assertTrue(selected_card.same_to(top_card))
+        self.assertTrue(selected_card.same_color(top_card))
 
     @parameterized.expand([
         (SkipCard(RED), WildCard()),
@@ -49,13 +48,13 @@ class TestUno(unittest.TestCase):
         (NumberCard(RED, '0'), WildCard()),
         (NumberCard(RED, '0'), DrawFourCard()),
     ])
-    def test_same_to_colored_card_valid_color_to_postcolored_card(
+    def test_same_color_valid_color_to_postcolored_card(
         self,
         selected_card,
         top_card
     ):
         top_card.set_color(RED)
-        self.assertTrue(selected_card.same_to(top_card))
+        self.assertTrue(selected_card.same_color(top_card))
 
     @parameterized.expand([
         (SkipCard(RED), NumberCard(YELLOW, '1')),
@@ -75,12 +74,12 @@ class TestUno(unittest.TestCase):
         (NumberCard(RED, '0'), ReverseCard(YELLOW)),
         (NumberCard(RED, '0'), DrawTwoCard(YELLOW)),
     ])
-    def test_same_to_colored_card_invalid_color_to_colored_card(
+    def test_same_color_invalid_color_to_colored_card(
         self,
         top_card,
         selected_card,
     ):
-        self.assertFalse(top_card.same_to(selected_card))
+        self.assertFalse(top_card.same_color(selected_card))
 
     @parameterized.expand([
         (SkipCard(RED), WildCard()),
@@ -92,13 +91,38 @@ class TestUno(unittest.TestCase):
         (NumberCard(RED, '0'), WildCard()),
         (NumberCard(RED, '0'), DrawFourCard()),
     ])
-    def test_same_to_colored_card_invalid_color_to_postcolored_card(
+    def test_same_color_invalid_color_to_postcolored_card(
         self,
         selected_card,
         top_card
     ):
         top_card.set_color(YELLOW)
-        self.assertFalse(selected_card.same_to(top_card))
+        self.assertFalse(selected_card.same_color(top_card))
+
+    @parameterized.expand([
+        (SkipCard(RED), SkipCard(GREEN)),
+        (ReverseCard(RED), ReverseCard(GREEN)),
+        (DrawTwoCard(RED), DrawTwoCard(GREEN)),
+    ])
+    def test_same_type_valid_colored_cards(self, selected_card, top_card):
+        self.assertTrue(selected_card.same_type(top_card))
+
+    @parameterized.expand([
+        (SkipCard(RED), NumberCard(GREEN, '1')),
+        (SkipCard(RED), ReverseCard(GREEN)),
+        (SkipCard(RED), DrawTwoCard(GREEN)),
+        (ReverseCard(RED), NumberCard(GREEN, '1')),
+        (ReverseCard(RED), SkipCard(GREEN)),
+        (ReverseCard(RED), DrawTwoCard(GREEN)),
+        (DrawTwoCard(RED), NumberCard(GREEN, '1')),
+        (DrawTwoCard(RED), SkipCard(GREEN)),
+        (DrawTwoCard(RED), ReverseCard(GREEN)),
+        (NumberCard(RED, '0'), SkipCard(GREEN)),
+        (NumberCard(RED, '0'), ReverseCard(GREEN)),
+        (NumberCard(RED, '0'), DrawTwoCard(GREEN)),
+    ])
+    def test_same_type_invalid_colored_cards(self, selected_card, top_card):
+        self.assertFalse(selected_card.same_type(top_card))
 
     @parameterized.expand([
         ('0',),
@@ -112,10 +136,10 @@ class TestUno(unittest.TestCase):
         ('8',),
         ('9',),
     ])
-    def test_number_card_same_number(self, number):
+    def test_same_number_valid_number_to_number_card(self, number):
         selected_card = NumberCard(RED, number)
         top_card = NumberCard(YELLOW, number)
-        self.assertTrue(selected_card.same_to(top_card))
+        self.assertTrue(selected_card.same_number(top_card))
 
     @parameterized.expand([
         ('0', '1'),
@@ -124,10 +148,94 @@ class TestUno(unittest.TestCase):
         ('6', '7'),
         ('8', '9'),
     ])
-    def test_number_same_to_number_no_equal(self, number1, number2):
+    def test_same_number_invalid_number_to_number_card(self, number1, number2):
         selected_card = NumberCard(RED, number1)
         top_card = NumberCard(YELLOW, number2)
-        self.assertFalse(selected_card.same_to(top_card))
+        self.assertFalse(selected_card.same_number(top_card))
+
+    @parameterized.expand([
+        (SkipCard(RED), NumberCard(RED, '1')),
+        (SkipCard(RED), SkipCard(YELLOW)),
+        (SkipCard(RED), ReverseCard(RED)),
+        (SkipCard(RED), DrawTwoCard(RED)),
+        (ReverseCard(RED), NumberCard(RED, '1')),
+        (ReverseCard(RED), SkipCard(RED)),
+        (ReverseCard(RED), ReverseCard(YELLOW)),
+        (ReverseCard(RED), DrawTwoCard(RED)),
+        (DrawTwoCard(RED), NumberCard(RED, '1')),
+        (DrawTwoCard(RED), SkipCard(RED)),
+        (DrawTwoCard(RED), ReverseCard(RED)),
+        (DrawTwoCard(RED), DrawTwoCard(YELLOW)),
+        (NumberCard(RED, '0'), NumberCard(YELLOW, '0')),
+        (NumberCard(RED, '0'), NumberCard(RED, '1')),
+        (NumberCard(RED, '0'), SkipCard(RED)),
+        (NumberCard(RED, '0'), ReverseCard(RED)),
+        (NumberCard(RED, '0'), DrawTwoCard(RED)),
+    ])
+    def test_is_valid_from_colored_card_to_valid_colored_card(
+        self,
+        selected_card,
+        top_card
+    ):
+        self.assertTrue(selected_card.is_valid(top_card))
+
+    @parameterized.expand([
+        (SkipCard(RED), WildCard()),
+        (SkipCard(RED), DrawFourCard()),
+        (ReverseCard(RED), WildCard()),
+        (ReverseCard(RED), DrawFourCard()),
+        (DrawTwoCard(RED), WildCard()),
+        (DrawTwoCard(RED), DrawFourCard()),
+        (NumberCard(RED, '0'), WildCard()),
+        (NumberCard(RED, '0'), DrawFourCard()),
+    ])
+    def test_is_valid_from_colored_card_to_valid_postcolored_card(
+        self,
+        selected_card,
+        top_card
+    ):
+        top_card.set_color(RED)
+        self.assertTrue(selected_card.is_valid(top_card))
+
+    @parameterized.expand([
+        (SkipCard(RED), NumberCard(YELLOW, '1')),
+        (SkipCard(RED), ReverseCard(YELLOW)),
+        (SkipCard(RED), DrawTwoCard(YELLOW)),
+        (ReverseCard(RED), NumberCard(YELLOW, '1')),
+        (ReverseCard(RED), SkipCard(YELLOW)),
+        (ReverseCard(RED), DrawTwoCard(YELLOW)),
+        (DrawTwoCard(RED), NumberCard(YELLOW, '1')),
+        (DrawTwoCard(RED), SkipCard(YELLOW)),
+        (DrawTwoCard(RED), ReverseCard(YELLOW)),
+        (NumberCard(RED, '0'), NumberCard(YELLOW, '1')),
+        (NumberCard(RED, '0'), SkipCard(YELLOW)),
+        (NumberCard(RED, '0'), ReverseCard(YELLOW)),
+        (NumberCard(RED, '0'), DrawTwoCard(YELLOW)),
+    ])
+    def test_is_valid_from_colored_card_to_invalid_colored_card(
+        self,
+        top_card,
+        selected_card,
+    ):
+        self.assertFalse(top_card.is_valid(selected_card))
+
+    @parameterized.expand([
+        (SkipCard(RED), WildCard()),
+        (SkipCard(RED), DrawFourCard()),
+        (ReverseCard(RED), WildCard()),
+        (ReverseCard(RED), DrawFourCard()),
+        (DrawTwoCard(RED), WildCard()),
+        (DrawTwoCard(RED), DrawFourCard()),
+        (NumberCard(RED, '0'), WildCard()),
+        (NumberCard(RED, '0'), DrawFourCard()),
+    ])
+    def test_is_valid_from_colored_card_to_invalid_postcolored_card(
+        self,
+        selected_card,
+        top_card
+    ):
+        top_card.set_color(YELLOW)
+        self.assertFalse(selected_card.is_valid(top_card))
 
     @parameterized.expand([
         (DrawFourCard(), NumberCard(RED, '1')),
@@ -143,5 +251,9 @@ class TestUno(unittest.TestCase):
         (WildCard(), DrawFourCard()),
         (WildCard(), WildCard()),
     ])
-    def test_same_to_postcolored_card_to_card(self, selected_card, top_card):
-        self.assertTrue(selected_card.same_to(top_card))
+    def test_is_valid_postcolored_card_to_any_card(
+        self,
+        selected_card,
+        top_card
+    ):
+        self.assertTrue(selected_card.is_valid(top_card))
