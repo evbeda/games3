3b1f269342456229ec7836bc92dc2e39b5ab51bc
Nahuel <nahuel.valencia@eventbrite.com>
2019-08-16 14:10:10 -0300

    Add level Logic. Monsters, Traps and Treasure parameterized.

--- dungeon_raiders/test_dungeon.py
+++ dungeon_raiders/test_dungeon.py
@@ -9,6 +9,7 @@ from .model.rooms.gold_room import GoldRoom
 from .model.rooms.wound_room import WoundRoom
 from .model.rooms.treasure import Treasure
 from .model.exceptions.UnplayableCardException import UnplayableCardException
+from .model.__init__ import GOLDS, WOUNDS
 
 
 class TestDungeon(unittest.TestCase):
@@ -93,11 +94,12 @@ class TestMonsterRoom(RoomHelper):
 
     """ -------------------- MonsterRoom card --------------------"""
     @parameterized.expand([
-        ([5, 3, 3, 5], MonsterRoom(14, 3), [5, 3, 1, 2]),
-        ([5, 3, 0, 5], MonsterRoom(14, 3), [5, 5, 4, 1]),
-        ([5, 6, 3, 8], MonsterRoom(14, 3), [3, 2, 2, 2]),
+        ([5, 3, 3, 5], MonsterRoom([14, 3, 'La Cosa']), [5, 3, 1, 2]),
+        ([5, 3, 0, 5], MonsterRoom([14, 3, 'sad']), [5, 5, 4, 1]),
+        ([5, 6, 3, 8], MonsterRoom([14, 3, 'gre']), [3, 2, 2, 2]),
     ])
-    def test_play_check_wounds_against_monster_room(self, players_wounds, monster, plays):
+    def test_play_check_wounds_against_monster_room(self, players_wounds,
+                                                    monster, plays):
         handA, handB, handC, handD = \
             self._play_cards_against_room(monster, plays)
         self.assertEqual(players_wounds, [
@@ -111,9 +113,13 @@ class TestTrapRoom(RoomHelper):
     """ -------------------- GoldRoom card --------------------"""
 
     @parameterized.expand([
-        ([3, 3, 0, 3], GoldRoom([0, 0, 1, 2, 3]), [3, 2, 4, 4]),
-        ([5, 3, 0, 5], GoldRoom([0, 0, 1, 2, 3]), [1, 1, 1, 1]),
-        ([2, 3, 0, 2], GoldRoom([0, 0, 1, 2, 3]), [5, 5, 5, 5])
+        ([4, 3, 0, 4], GoldRoom(GOLDS[0]), [3, 1, 2, 3]),
+        ([3, 3, 0, 3], GoldRoom(GOLDS[0]), [4, 1, 2, 3]),
+        ([2, 3, 0, 2], GoldRoom(GOLDS[0]), [5, 2, 3, 4]),
+        ([4, 3, 0, 4], GoldRoom(GOLDS[1]), [2, 1, 2, 1]),
+        ([4, 3, 0, 4], GoldRoom(GOLDS[1]), [3, 2, 1, 2]),
+        ([3, 3, 0, 3], GoldRoom(GOLDS[1]), [4, 1, 3, 4]),
+        ([3, 3, 0, 3], GoldRoom(GOLDS[1]), [5, 5, 4, 3]),
     ])
     def test_play_cards_against_gold_room(self, gold_values, room, plays):
         handA, handB, handC, handD = \
@@ -124,9 +130,13 @@ class TestTrapRoom(RoomHelper):
 
     """ -------------------- WoundRoom card -------------------- """
     @parameterized.expand([
-        ([5, 3, 2, 5], WoundRoom([0, 0, 1, 2, 2]), [3, 2, 4, 4]),
-        ([5, 3, 0, 5], WoundRoom([0, 0, 1, 2, 2]), [1, 2, 2, 1]),
-        ([5, 3, 2, 5], WoundRoom([0, 0, 1, 2, 2]), [5, 4, 3, 2]),
+        ([5, 3, 1, 5], WoundRoom(WOUNDS[0]), [3, 1, 1, 2]),
+        ([5, 3, 2, 5], WoundRoom(WOUNDS[0]), [4, 1, 2, 3]),
+        ([5, 3, 2, 5], WoundRoom(WOUNDS[0]), [5, 2, 3, 4]),
+        ([5, 3, 1, 5], WoundRoom(WOUNDS[1]), [2, 1, 1, 2]),
+        ([5, 3, 1, 5], WoundRoom(WOUNDS[1]), [3, 1, 2, 3]),
+        ([5, 3, 2, 5], WoundRoom(WOUNDS[1]), [4, 1, 2, 3]),
+        ([5, 3, 2, 5], WoundRoom(WOUNDS[1]), [5, 2, 3, 4])
     ])
     def test_play_cards_against_wound_room(self, wound_values, room, plays):
         handA, handB, handC, handD = \
@@ -160,7 +170,6 @@ class TestTreasure(RoomHelper):
 
 
 class TestLevel(unittest.TestCase):
-
     def test_check_if_each_levels_has_five_rooms(self):
-        level = Level([Player('A'), Player('B'), Player('C'), Player('D')])
+        level = Level([Player('A'), Player('B'), Player('C')])
         self.assertEqual(5, len(level.rooms))
