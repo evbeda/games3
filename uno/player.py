cd4485d40b2f0cfe570a32060435b89080b88a6b
Walter Bucolo <walter.bucolo@eventbrite.com>
2019-08-16 16:16:40 -0300

    recreate_stack, put_card_in_discard, generate_cards_player added print board and winner player

--- uno/player.py
+++ uno/player.py
@@ -3,9 +3,12 @@
 class Player():
 
     def __init__(self, stack):
-        self.cards_player = stack.generated_cards_player()  
+        self.cards_player = stack.generated_cards_player()
 
     def selected_card(self, index_card):
         for index, value in enumerate(self.cards_player):
             if index == index_card:
                 return value
+
+    def show_player_cards(self):
+        return self.cards_player
