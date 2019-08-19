e5d1a2c08e64fc102930ab71fb4698a3dc2a0b72
julianandres <julian.andres@eventbrite.com>
2019-08-19 10:32:11 -0300

    Refactor of Treasure

--- dungeon_raiders/model/rooms/treasure.py
+++ dungeon_raiders/model/rooms/treasure.py
@@ -6,39 +6,42 @@ class Treasure(Room):
     def __init__(self, values):
         self.values = values
 
-    def resolve_room(self, hands):
-        played_cards = [hand.last_card_played for hand in hands]
-        cards_second_treasure = []
-        max_value = max(played_cards)
-        first_treasure_winners = [
+    # Determine winners
+    def determine_winners(self, max_value, played_cards):
+        return [
             (max_value, index) for index, max_value in enumerate(played_cards)
             if max_value == max(played_cards)
             ]
-        for card in played_cards:
-            if card == max_value:
-                cards_second_treasure.append(0)
-            else:
-                cards_second_treasure.append(card)
-        second_treasure_winners = [
-            (max_value, index) for index,
-            max_value in enumerate(cards_second_treasure)
-            if max_value == max(cards_second_treasure)
-            ]
 
-        first_winners = len(first_treasure_winners)
-        first_treasure = self.values[0]
-        first_reward = first_treasure // first_winners
-        for fst_winners in first_treasure_winners:
+    def add_treasure(self, treasure_winners, hands, who):
+        winners = len(treasure_winners)
+        treasure = self.values[who]
+        reward = treasure // winners
+        for winner in treasure_winners:
             for hand in hands:
-                if fst_winners[1] == hands.index(hand):
+                if winner[1] == hands.index(hand):
                     player = hand.player
-                    player.add_gold(first_reward)
+                    player.add_gold(reward)
 
-        second_winners = len(second_treasure_winners)
-        second_treasure = self.values[1]
-        second_reward = second_treasure // second_winners
-        for snd_winners in second_treasure_winners:
-            for hand in hands:
-                if snd_winners[1] == hands.index(hand):
-                    player = hand.player
-                    player.add_gold(second_reward)
+    def resolve_room(self, hands):
+        # For example: played_cards = [5, 3, 1, 1]
+        played_cards = [hand.last_card_played for hand in hands]
+        cards_second_treasure = []
+        # max_value = 5
+        max_value = max(played_cards)
+        # Determine first winners. first_treasure_winners = [(5, 0)]
+        first_treasure_winners = \
+            self.determine_winners(max_value, played_cards)
+        # cards_second_treasure = [0, 3, 1, 1]
+        cards_second_treasure = [
+            card
+            if card < max_value else 0
+            for card in played_cards
+        ]
+        # Determine second winners. second_treasure_winners = [(3, 1)]
+        second_treasure_winners = \
+            self.determine_winners(max_value, cards_second_treasure)
+        # Set new treasure value to winners
+        # 0 means first winners and 1 second winners
+        self.add_treasure(first_treasure_winners, hands, 0)
+        self.add_treasure(second_treasure_winners, hands, 1)
