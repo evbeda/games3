e29ce4fd5b93272b4248cb107964362bfc14a0b3
Walter Bucolo <walter.bucolo@eventbrite.com>
2019-08-19 12:52:29 -0300

    refactor of game class

--- dungeon_raiders/model/game.py
+++ dungeon_raiders/model/game.py
@@ -1,19 +1,47 @@
 from .level import Level
+from ..model.__init__ import CHARACTER
+from .player import Player
+import random
 
 
 class Game:
-    def __init__(self, players):
-        self.players = players
+    def __init__(self):
+        self.players = []
+        self.create_players()
+        self.create_levels()
+
+    def create_levels(self):
         self.levels = [
-            Level(players),
-            Level(players),
-            Level(players),
-            Level(players),
-            Level(players)
+            Level(self.players),
+            Level(self.players),
+            Level(self.players),
+            Level(self.players),
+            Level(self.players)
             ]
+
+    def create_players(self):
+        characters = []
+        list_shuffle = list(range(0, 5))
+        random.shuffle(list_shuffle)
+        for k, v in enumerate(list_shuffle):
+            characters.append(CHARACTER[v])
+            self.players.append(Player('', characters[k]))
+            if len(characters) == 3:
+                break
 
     def get_winner(self):
         self.players.sort(key=lambda elem: elem.wounds)
         self.players.pop()
         self.players.sort(key=lambda elem: elem.gold, reverse=True)
         return self.players[0]
+
+    def next_turn(self):
+        pass    
+
+    def play(self, number_card):
+
+        pass
+
+    @property
+    def board(self):
+        pass
--- dungeon_raiders/test_dungeon.py
+++ dungeon_raiders/test_dungeon.py
@@ -40,21 +40,18 @@ class TestDungeon(unittest.TestCase):
 
     """ -------------------- Game tests -------------------- """
     def test_check_if_game_has_5_levels(self):
-        game = Game([Player('A'), Player('B'), Player('C'), Player('D')])
+        game = Game()
         self.assertEqual(5, len(game.levels))
 
     def test_check_if_player_a_wins(self):
-        playerA = Player('A')
-        playerB = Player('B')
-        playerC = Player('C')
-        playerA.gold = 50
-        playerA.wounds = 10
-        playerB.gold = 40
-        playerB.wounds = 15
-        playerC.gold = 60
-        playerC.wounds = 20
-        game = Game([playerA, playerB, playerC])
-        self.assertEqual(playerA, game.get_winner())
+        game = Game()
+        game.players[0].gold = 50
+        game.players[0].wounds = 10
+        game.players[1].gold = 40
+        game.players[1].wounds = 15
+        game.players[2].gold = 60
+        game.players[2].wounds = 20    
+        self.assertEqual(game.players[0], game.get_winner())
 
     def _play(self, room, hands):
         room.resolve_room(hands)
