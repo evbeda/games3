33d4476d1c90ec9cac77437137642d67603144df
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-08-19 15:11:12 -0300

    ruleta: uppercase constants, useless method removed

--- ruleta/board.py
+++ ruleta/board.py
@@ -1,5 +1,5 @@
 
-board = [
+BOARD = [
    [0, 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34],
    [0, 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35],
    [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36],
@@ -18,14 +18,10 @@ def get_dozen_from_number(number):
 def get_color_from_number(number):
     if number == 0:
         return 'green'
-    if number > 0 or number < 37:
+    if 0 < number < 37:
         range_1 = (0 < number <= 10 and number % 2 == 1)
         range_2 = (11 <= number <= 18 and number % 2 == 0)
         range_3 = (19 <= number <= 28 and number % 2 == 1)
         range_4 = (29 <= number <= 36 and number % 2 == 0)
         return 'red' if range_1 or range_2 or range_3 or range_4 \
             else 'black'
-
-
-def get_odd_from_number(self, number):
-    return True if number % 2 == 1 else False
