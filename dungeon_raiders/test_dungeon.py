dd7be753bd8efebb16bb60494d1f4809b792b321
walterbucolo-eb <53877957+walterbucolo-eb@users.noreply.github.com>
2019-08-19 09:51:20 -0300

    Revert "test_check_if_level_has_hands_for_player added"
    This reverts commit cd078efd573183b56707fc4dc1048360c06d5c83.

--- dungeon_raiders/test_dungeon.py
+++ dungeon_raiders/test_dungeon.py
@@ -173,8 +173,3 @@ class TestLevel(unittest.TestCase):
     def test_check_if_each_levels_has_five_rooms(self):
         level = Level([Player('A'), Player('B'), Player('C')])
         self.assertEqual(5, len(level.rooms))
-
-    def test_check_if_level_has_hands_for_player(self):
-        level = Level([Player('A'), Player('B'), Player('C')])
-        self.assertEqual(3, len(level.hands))
-
