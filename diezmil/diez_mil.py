1ad4f8c2902bb99d25e94ed4463cfa59edb4ce7e
Juan Pablo Vallejo <juan.vallejo@eventbrite.com>
2019-08-20 15:48:28 -0300

    Refactored diez mil and fixed tests

--- diezmil/diez_mil.py
+++ diezmil/diez_mil.py
@@ -1,4 +1,5 @@
 from .player import Player
+from .turn import Turn
 from . import (
     SETUP,
     GO,
@@ -30,48 +31,51 @@ class DiezMil(object):
     # It's a circle, it adds one until the top boundary of players_qty
     # Otherwise it beging with the first one
     def next_turn(self):
-        if self.players_qty == 0:
-            return PLAYERS_QUANTITY_MESSAGE
-        elif self.state == SETUP and self.players_qty != 0:
+        if self.state == SETUP:
             return PLAYERS_NAME_MESSAGE
-        elif self.state == GO and self.actual_turn.state == PLAYING:
+        elif self.state == GO and self.actual_turn.is_playing():
             if self.actual_turn.plays[-1].is_playing:
                 return PLAYER_CHOOSING_DICE_MESSAGE
             else:
+                # innecesary?
                 return PLAYER_DONE_MESSAGE
-        elif self.state == GO and self.actual_turn.state == FINISHED:
+        elif self.state == GO and not self.actual_turn.is_playing():
             return NEXT_PLAYER_TURN_MESSAGE
 
     def next_player(self):
-        if self.state == SETUP:
-            self.state = GO
-        elif self.state == GO:
-            self.who_is_playing = (self.who_is_playing + 1) \
-                if self.who_is_playing in range(1, self.players_qty) else 1
+        self.who_is_playing = (self.who_is_playing + 1) \
+            if self.who_is_playing in range(1, self.players_qty) else 1
+        # import pdb; pdb.set_trace()
+        self.actual_turn = Turn(self.players[self.who_is_playing - 1])
 
     def check_players_qty(self, players_qty):
         return False if players_qty == 0 else True
 
-    def validate_qty(self, response):
-        if response not in list(range(1, 6)):
-            raise NotCorrectPlayersQuantityException(INVALID_NUMBER)
+    # def validate_qty(self, response):
+    #     if response not in list(range(1, 6)):
+    #         raise NotCorrectPlayersQuantityException(INVALID_NUMBER)
 
     def create_players(self, names):
         for name in names:
             self.players.append(Player(name))
+        self.players_qty = len(names)
 
     def play(self, player_input):
-        if self.players_qty == 0:
-            try:
-                self.validate_qty(player_input)
-            except NotCorrectPlayersQuantityException as e:
-                return str(e)
-            self.players_qty = int(player_input)
-            return QUANTITY_SET
-        elif self.state == SETUP and self.players_qty != 0:
+
+        if self.state == SETUP:
             names = player_input.split(',')
+            self.state = GO
             self.create_players(names)
+            self.next_player()
             return PLAYERS_SET
+        elif player_input == 'STAY':
+            # asignar puntaje
+            self.actual_turn.calculate_acumulated_score()
+            self.next_player()
+        else:
+            self.actual_turn.select_dices(player_input)
+            if not self.actual_turn.is_playing():  # Loses the turn
+                self.next_player()
 
     # Por cada jugador, mostrar su puntaje
     # @property
