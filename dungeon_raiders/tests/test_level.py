7541bbb5135693c2f9b097d206b36c7432abf562
juan.elias <juan.elias@juan.elias-10970>
2019-08-20 11:32:25 -0300

    refactored and added level tests

--- dungeon_raiders/tests/test_level.py
+++ dungeon_raiders/tests/test_level.py
@@ -4,8 +4,21 @@ from ..model.player import Player
 
 
 class TestLevel(unittest.TestCase):
+    def setUp(self):
+        self.players = [
+            Player('A'),
+            Player('B'),
+            Player('C'),
+        ]
+        self.level = Level(self.players)
+
     def test_check_if_each_levels_has_five_rooms(self):
-        level = Level([Player('A'), Player('B'), Player('C')])
-        self.assertEqual(5, len(level.rooms))
+        self.assertEqual(5, len(self.level.rooms))
 
+    def test_create_hands_for_level(self):
+        result = self.level.create_hands_for_level(self.players)
+        self.assertEqual(self.players, [hand.player for hand in result])
 
+    def test_select_rooms(self):
+        # TODO: refactor room deck handling
+        pass
