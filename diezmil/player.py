b3937a709546ae5b5b3a7aae1e77bb5443c20f1f
dbertranou-eb <daniela.bertranou@eventbrite.com>
2019-08-20 12:10:24 -0300

    ded diez mil play and next turn tests

--- diezmil/player.py
+++ diezmil/player.py
@@ -5,7 +5,3 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.actual_score = 0
-
-    def __gt__(self, player):
-        return self.name == player.name and \
-            self.actual_score == player.actual_score
