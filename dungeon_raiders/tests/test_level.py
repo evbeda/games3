7a00b4d5b8d4452ca1b2d3b11f503b714ef12a70
nicolas.nunez <nicolas.nunez@nicolas.nunez-07221>
2019-08-20 17:27:04 -0300

    Execute level method added with tests

--- dungeon_raiders/tests/test_level.py
+++ dungeon_raiders/tests/test_level.py
@@ -1,16 +1,17 @@
-import unittest
+# Modules
+from parameterized import parameterized
+# Model
 from ..model.level import Level
-from ..model.player import Player
 from ..model import ROOMS
+from ..model.rooms.monster_room import MonsterRoom
+from ..model.rooms.treasure import Treasure
+# Helpers
+from . import RoomHelper
 
 
-class TestLevel(unittest.TestCase):
+class TestLevel(RoomHelper):
     def setUp(self):
-        self.players = [
-            Player('A'),
-            Player('B'),
-            Player('C'),
-        ]
+        self.players = self._get_players_example()
         self.deck = ROOMS.copy()
         self.level = Level(self.players, 1, self.deck)
 
@@ -24,3 +25,22 @@ class TestLevel(unittest.TestCase):
     def test_select_rooms(self):
         # TODO: refactor room deck handling
         pass
+
+# PLAYERS_EXAMPLE = [             wounds, gold
+#    Player(character=['Caballero', 1, 1]),
+#    Player(character=['Exploradora', 3, 2]),
+#    Player(character=['Guerrero', 2, 0]),
+# ]
+    @parameterized.expand([
+        ([1, 4, 5], 0, 'wounds', MonsterRoom((11, 3, 'Esqueleto')), 4),
+        ([3, 2, 3], 0, 'gold', Treasure((4, 2)), 3),
+        ([3, 2, 3], 2, 'gold', Treasure((4, 2)), 2)
+    ])
+    def test_execute_level(
+        self, cards_played, player_affected, attribute_affected, actual_room, expected_effect
+            ):
+        level = Level(self.players, 1, ROOMS[:])
+        level.actual_room = actual_room
+        level.execute_level(cards_played)
+        self.assertEqual(
+            getattr(self.players[player_affected], attribute_affected), expected_effect)
