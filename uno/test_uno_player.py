337a103c53eda68336b0a9eaa1e92070037b5887
Juan Pablo Vallejo <juan.vallejo@eventbrite.com>
2019-08-20 11:42:54 -0300

    added test for auto play

--- uno/test_uno_player.py
+++ uno/test_uno_player.py
@@ -4,6 +4,7 @@ from .uno import Uno
 from .card import (
     NumberCard,
     WildCard,
+    DrawTwoCard,
 )
 from .const import GREEN, RED, BLUE
 
@@ -33,6 +34,28 @@ class TestPlayerUno(unittest.TestCase):
         uno.computer_player.cards_player.append(wildcard)
         card = uno.computer_player.auto_play(uno.stack.discard_cards[-1])
         self.assertTrue(card.is_valid(uno.stack.discard_cards[-1]))
+
+    def test_auto_play_after_draw_to(self):
+        uno = Uno()
+        draw_two = DrawTwoCard(GREEN)
+        uno.computer_player.cards_player.append(draw_two)
+        card = uno.computer_player.auto_play(draw_two)
+        self.assertTrue(isinstance(card, DrawTwoCard))
+
+    def test_auto_play_after_draw_to_if_not_in_hand(self):
+        """If the player don't have a draw to card, he can't play a card"""
+        uno = Uno()
+        draw_two = DrawTwoCard(GREEN)
+        uno.computer_player.cards_player = [NumberCard(GREEN, 3)]
+        card = uno.computer_player.auto_play(draw_two)
+        self.assertTrue(card is None)
+
+    def test_auto_play_if_has_not_valid_card(self):
+        """If the player don't have a valid card, he can't play a card"""
+        uno = Uno()
+        uno.computer_player.cards_player = [NumberCard(GREEN, 3)]
+        card = uno.computer_player.auto_play(NumberCard(RED, 5))
+        self.assertTrue(card is None)
 
     def test_selected_card(self):
         card1 = NumberCard(GREEN, 3)
