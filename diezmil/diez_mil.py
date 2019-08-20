5dae082f836ad98ed2602d260f94f828b233d55a
Nahuel <nahuel.valencia@eventbrite.com>
2019-08-20 11:19:03 -0300

    Next Turn and Play functions refactored

--- diezmil/diez_mil.py
+++ diezmil/diez_mil.py
@@ -1,10 +1,21 @@
 from .player import Player
-from .turn import Turn
-from .__init__ import SETUP, GO
-from .exceptions.exceptions import NegativePlayersQuantityException, \
-    NullPlayersQuantityException, DifferentPlayerQuantityAndNamesException, \
-    ManyPlayersQuantityException, NotCorrectPlayersQuantityException, \
-    NotANumberException
+from . import (
+    SETUP,
+    GO,
+    PLAYERS_QUANTITY_MESSAGE,
+    PLAYING,
+    FINISHED,
+    PLAYER_CHOOSING_DICE_MESSAGE,
+    PLAYER_DONE_MESSAGE,
+    PLAYERS_NAME_MESSAGE,
+    CHOOSING_DICE,
+    NEXT_PLAYER_TURN_MESSAGE,
+    QUANTITY_SET,
+    INVALID_NUMBER,
+)
+from .exceptions.exceptions import (
+    NotCorrectPlayersQuantityException,
+)
 
 
 class DiezMil(object):
@@ -18,6 +29,19 @@ class DiezMil(object):
 
     # It's a circle, it adds one until the top boundary of players_qty
     # Otherwise it beging with the first one
+    def next_turn(self):
+        if self.state == SETUP and self.players_qty == 0:
+            return PLAYERS_QUANTITY_MESSAGE
+        elif self.state == SETUP and self.players_qty != 0:
+            return PLAYERS_NAME_MESSAGE
+        elif self.state == GO and self.actual_turn.state == PLAYING:
+            if self.actual_turn.plays[-1] == CHOOSING_DICE:
+                return PLAYER_CHOOSING_DICE_MESSAGE
+            else:
+                return PLAYER_DONE_MESSAGE
+        elif self.state == GO and self.actual_turn == FINISHED:
+            return NEXT_PLAYER_TURN_MESSAGE
+
     def next_player(self):
         if self.state == SETUP:
             self.who_is_playing = 1
@@ -26,63 +50,28 @@ class DiezMil(object):
             self.who_is_playing = (self.who_is_playing + 1) \
                 if self.who_is_playing in range(1, self.players_qty) else 1
 
-    def players_creator(self, players_quantity, names):
-        if players_quantity < 0:
-            raise NegativePlayersQuantityException
-        elif players_quantity == 0:
-            raise NullPlayersQuantityException
-        elif players_quantity >= 6:
-            raise ManyPlayersQuantityException
-        elif players_quantity in list(range(1, 6)):
-            if len(names) == players_quantity:
-                for name in names:
-                    self.players.append(Player(name))
-            else:
-                raise DifferentPlayerQuantityAndNamesException
-        return self.players
-
     def check_players_qty(self, players_qty):
         return False if players_qty == 0 else True
 
-    def ask_for_players_quantity(self):
-        asking_ended = False
-        while asking_ended is False:
-            response = input("How many players?: (A number between 1 and 5) ")
-            possible_quantity = 0
-            try:
-                possible_quantity = int(response)
-            except ValueError:
-                raise NotANumberException()
-            if possible_quantity in list(range(1, 6)):
-                self.set_players_qty(possible_quantity)
-                asking_ended = True
-            else:
-                raise NotCorrectPlayersQuantityException()
+    def validate_qty(self, response):
+        if response not in list(range(1, 6)):
+            raise NotCorrectPlayersQuantityException(INVALID_NUMBER)
 
-    def create_players(self):
-        for i in list(range(1, self.players_qty + 1)):
-            name = input("What's the name of player " + str(i))
+    def create_players(self, names):
+        for name in names:
             self.players.append(Player(name))
 
-    def play(self, command):
-        if self.state == SETUP:
-            # Ask for players's quantity
-            self.ask_for_players_quantity()
-            # Create players
-            self.create_players()
-            # Who's next?
-            self.next_turn()
-        elif self.state == GO:
-            # Crear un turno para el jugador y que empiecen las apuestas
-            if self.actual_turn:
-                self.actual_turn = Turn(self.players[self.who_is_playing])
-
-            command = self.resolve_command(command)
-            self.actual_turn.generate_play()
-
-            # El primer jugador jugo pero no termino
-            # El primer jugador decide terminar
-            # Comienza el siguiente jugador
+    def play(self, player_input):
+        if self.state == SETUP and self.players_qty == 0:
+            try:
+                self.validate_qty(player_input)
+            except NotCorrectPlayersQuantityException as e:
+                return str(e)
+            self.players_qty = player_input
+            return QUANTITY_SET
+        elif self.state == SETUP and self.players_qty != 0:
+            names = player_input.split(',')
+            self.create_players(names)
 
     # Por cada jugador, mostrar su puntaje
     @property
