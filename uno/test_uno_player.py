6c37c7d025ea38022519e9d95f91e9ec4327c2bc
dbertranou-eb <daniela.bertranou@eventbrite.com>
2019-08-22 11:59:55 -0300

    added human select_card

--- uno/test_uno_player.py
+++ uno/test_uno_player.py
@@ -1,61 +1,67 @@
 import unittest
-from .player import Player
-from .uno import Uno
+from .player import HumanPlayer
+from .stack import Stack
 from .card import (
     NumberCard,
-    WildCard,
-    DrawTwoCard,
+    # WildCard,
+    # DrawTwoCard,
 )
-from .const import GREEN, RED, BLUE
+from .const import GREEN, RED, INVALID_CARD_MESSAGE
 
 
 class TestPlayerUno(unittest.TestCase):
+    def setUp(self):
+        self.player = HumanPlayer([])
+        self.stack = Stack()
 
-    def test_add_cards_to_hand(self):
-        player = Player([])
-        card = NumberCard(GREEN, 2)
-        player.add_cards_to_hand([card])
-        self.assertTrue(card in player.cards_player)
+    def test_human_select_valid_colored_card(self):
+        valid_card = NumberCard(GREEN, 2)
+        self.stack.discard_cards = [valid_card]
+        self.player.cards_player = [valid_card]
+        card = self.player.select_card(0, self.stack)
+        self.assertEqual(card, valid_card)
 
-    def test_add_multiple_cards_to_hand(self):
-        player = Player([])
-        card1 = NumberCard(GREEN, 3)
-        card2 = NumberCard(RED, 4)
-        card3 = NumberCard(BLUE, 5)
-        cards = [card1, card2, card3]
-        player.add_cards_to_hand(cards)
-        self.assertTrue(all(card in player.cards_player for card in cards))
+    def test_human_select_invalid_colored_card(self):
+        top_card = NumberCard(RED, 5)
+        invalid_card = NumberCard(GREEN, 2)
+        self.stack.discard_cards = [top_card]
+        self.player.cards_player = [invalid_card]
+        with self.assertRaises(Exception):
+            self.player.select_card(0, self.stack)
+        with self.assertRaises(Exception):
+            self.player.select_card(1, self.stack)
 
-    def test_auto_play(self):
-        uno = Uno()
-        wildcard = WildCard()
-        uno.stack.discard_cards.append(NumberCard(GREEN, 4))
-        uno.computer_player.cards_player.pop(0)
-        uno.computer_player.cards_player.append(wildcard)
-        card = uno.computer_player.auto_play(uno.stack.discard_cards[-1])
-        self.assertTrue(card.is_valid(uno.stack.discard_cards[-1]))
 
-    def test_auto_play_after_draw_to(self):
-        uno = Uno()
-        draw_two = DrawTwoCard(GREEN)
-        uno.computer_player.cards_player.append(draw_two)
-        card = uno.computer_player.auto_play(draw_two)
-        self.assertTrue(isinstance(card, DrawTwoCard))
+    # def test_auto_play(self):
+    #     uno = Uno()
+    #     wildcard = WildCard()
+    #     uno.stack.discard_cards.append(NumberCard(GREEN, 4))
+    #     uno.computer_player.cards_player.pop(0)
+    #     uno.computer_player.cards_player.append(wildcard)
+    #     card = uno.computer_player.auto_play(uno.stack.discard_cards[-1])
+    #     self.assertTrue(card.is_valid(uno.stack.discard_cards[-1]))
 
-    def test_auto_play_after_draw_to_if_not_in_hand(self):
-        """If the player don't have a draw to card, he can't play a card"""
-        uno = Uno()
-        draw_two = DrawTwoCard(GREEN)
-        uno.computer_player.cards_player = [NumberCard(GREEN, 3)]
-        card = uno.computer_player.auto_play(draw_two)
-        self.assertTrue(card is None)
+    # def test_auto_play_after_draw_to(self):
+    #     uno = Uno()
+    #     draw_two = DrawTwoCard(GREEN)
+    #     uno.computer_player.cards_player.append(draw_two)
+    #     card = uno.computer_player.auto_play(draw_two)
+    #     self.assertTrue(isinstance(card, DrawTwoCard))
 
-    def test_auto_play_if_has_not_valid_card(self):
-        """If the player don't have a valid card, he can't play a card"""
-        uno = Uno()
-        uno.computer_player.cards_player = [NumberCard(GREEN, 3)]
-        card = uno.computer_player.auto_play(NumberCard(RED, 5))
-        self.assertTrue(card is None)
+    # def test_auto_play_after_draw_to_if_not_in_hand(self):
+    #     """If the player don't have a draw to card, he can't play a card"""
+    #     uno = Uno()
+    #     draw_two = DrawTwoCard(GREEN)
+    #     uno.computer_player.cards_player = [NumberCard(GREEN, 3)]
+    #     card = uno.computer_player.auto_play(draw_two)
+    #     self.assertTrue(card is None)
+
+    # def test_auto_play_if_has_not_valid_card(self):
+    #     """If the player don't have a valid card, he can't play a card"""
+    #     uno = Uno()
+    #     uno.computer_player.cards_player = [NumberCard(GREEN, 3)]
+    #     card = uno.computer_player.auto_play(NumberCard(RED, 5))
+    #     self.assertTrue(card is None)
 
     # def test_selected_card(self):
     #     card1 = NumberCard(GREEN, 3)
--- uno/uno.py
+++ uno/uno.py
@@ -1,6 +1,6 @@
 from .const import DRAW_CARD_INPUT, EXIT, INVALID_CARD_MESSAGE
 from .stack import Stack
-from .player import Player
+from .player import HumanPlayer, Player
 from .const import ASK_FOR_INPUT
 
 
@@ -12,11 +12,8 @@ class Uno():
     def __init__(self):
         self.is_playing = True
         self.stack = Stack()
-        self.player = Player(self.stack.generate_cards_player())
-        self.computer_player = Player(
-            self.stack.generate_cards_player(),
-            'Computer_player'
-            )
+        self.player = HumanPlayer(self.stack.generate_cards_player())
+        self.computer_player = Player(self.stack.generate_cards_player())
         self.stack.put_card_in_discard()
         self.current_player = self.player
 
@@ -31,39 +28,11 @@ class Uno():
         elif command == DRAW_CARD_INPUT:
             self.player_draws(self.player)
         else:
-            card = self.current_player.selected_card(command)
-            if not card.is_valid(self.stack.discard_cards[-1]):
+            parsed = self.parse_command(command)
+            try:
+                self.current_player.play(parsed, self.stack)
+            except Exception:
                 return INVALID_CARD_MESSAGE
-            self.player_plays_card(self.player, card)
-
-        # if not self.player.loses_turn:
-            # if command == EXIT:
-                # self.is_playing = False
-                # return 'Bye'
-            # elif command == DRAW_CARD_INPUT:
-                # return self.player.cards_player.append(
-                    # self.stack.stack_cards.pop())
-            # else:
-                # card = self.player.selected_card(command)
-                # is_valid_card = card.is_valid(self.stack.discard_cards[-1])
-                # if is_valid_card is False:
-                    # return "Your card is not valid"
-                # else:
-                    # self.player.cards_player.remove(card)
-                    # self.stack.put_card_in_discard(card)
-                    # # Get card action. Maybe put in other function
-                    # lose_turn, cards_to_pick = card.get_action()
-                    # if lose_turn:
-                        # self.computer_player.loses_turn = True
-                    # return self.winner(self.player)
-        # if not self.computer_player.loses_turn:
-            # card = self.computer_player.auto_play(self.stack.discard_cards[-1])
-            # if card is not None:
-                # self.stack.put_card_in_discard(card)
-                # return self.winner(self.computer_player)
-            # else:
-                # return self.computer_player.cards_player.append(
-                    # self.stack.stack_cards.pop())
 
     def player_draws(self, player):
         player.cards_player.append(self.stack.stack_cards.pop())
