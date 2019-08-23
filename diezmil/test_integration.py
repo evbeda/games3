4f56d605a39aa2c9cd971ced8e5d6131b311eead
Agustín Dávila <agustin.davila@agustin.davila-07031>
2019-08-23 12:53:58 -0300

    test_board

--- diezmil/test_integration.py
+++ diezmil/test_integration.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 from .diez_mil import DiezMil
 from .turn import Turn
 from .play import Play
+from .player import Player
 from . import (
     SETUP,
     GO,
@@ -14,6 +15,7 @@ from . import (
     PLAYERS_SET,
     CANT_SAVE_THOSE_DICES,
     )
+
 
 class IntegrationTest(TestCase):
     # Test for the game diez mil
@@ -32,6 +34,7 @@ class IntegrationTest(TestCase):
         self.game.state = state
         self.assertEqual(message, self.game.next_turn())
 
+<<<<<<< HEAD
     # @parameterized.expand([
     #     ('STAY'),
     #     ('0,1'),
@@ -39,4 +42,29 @@ class IntegrationTest(TestCase):
     # ])
     # def test_play_stay(self):
     #     self.game.next_player()
+=======
+    @parameterized.expand([
+        ('STAY'),
+        ('0,1'),
+        (GO, 'Next player turn'),
+    ])
+    def test_play(self):
+        pass
 
+    def test_show_board(self):
+        test_board = 'Player: Agustin\n'
+        test_board += 'Score: 0\n'
+        for play in self.plays:
+            test_board += '====================\n'
+            test_board += 'Play:\n'
+            test_board += 'Dices: [1, 1, 3, 5, 5]\n'
+            test_board += 'Play score 300'
+        player = Player('Agustin')
+        turn = Turn(player)
+        play = Play()
+        play.play_score = 300
+        play.dices = [1, 1, 3, 5, 5]
+        turn.plays.append(play)
+>>>>>>> test_board
+
+        self.assertEqual(test_board, turn.build_board)
