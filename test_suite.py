41e8b61540285a4390951133bd79254f0618697a
Juan Pablo Vallejo <juan.vallejo@eventbrite.com>
2019-08-23 10:03:04 -0300

    Added test suite for roulette game

--- test_suite.py
+++ test_suite.py
@@ -4,17 +4,8 @@ from craps.craps_suite import suite as craps_suite
 from dungeon_raiders.tests.test_suite_dungeon import suite as dr_suite
 from sudoku.tests.test_suite_sudoku import suite as sudoku_suite
 from guess_number_game.test_guess_number_game import TestGuessNumberGame
-from ruleta.tests.test_ruleta import (
-    TestRoulette,
-    TestBoard
-)
-from ruleta.tests.test_bets import (
-    TestBetsRoulette,
-    TestBetCreator,
-)
-from ruleta.tests.test_croupier import TestCroupier
-from ruleta.tests.integration_test import IntegrationTest
 from diezmil.test_diez_mil import TestDiezMil
+from ruleta.tests.test_roulette_suite import suite as roulette_suite
 from test_game import TestGame
 
 
@@ -25,12 +16,7 @@ def suite():
     # CRAPS
     test_suite.addTest(craps_suite())
     # ROULETTE
-    test_suite.addTest(unittest.makeSuite(TestBetsRoulette))
-    test_suite.addTest(unittest.makeSuite(TestBetCreator))
-    test_suite.addTest(unittest.makeSuite(TestRoulette))
-    test_suite.addTest(unittest.makeSuite(TestCroupier))
-    test_suite.addTest(unittest.makeSuite(IntegrationTest))
-    test_suite.addTest(unittest.makeSuite(TestBoard))
+    test_suite.addTest(roulette_suite())
     # SUDOKU
     test_suite.addTest(sudoku_suite())
     # DANGEONS RAIDERS
